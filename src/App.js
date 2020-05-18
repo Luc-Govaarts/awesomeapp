160eb87ace00b9fdaa60832e976b046ded96461d
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-05-18 11:46:08 +0200

    Build Title Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
-      <p>Hi there react enthousiasts</p>
-      <h1>Come and play</h1>
+      <main>
+        <Title />
+      </main>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
